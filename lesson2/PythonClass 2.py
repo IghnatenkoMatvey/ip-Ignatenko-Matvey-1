b41d97b60b4499810dbf02801631d46facbf7de4
IgnatenkoMatvey <matvej.ignatenko05@mail.ru>
2020-08-12 20:38:37 +0400

    lesson3

--- lesson2/PythonClass 2.py
+++ lesson2/PythonClass 2.py
@@ -4,6 +4,43 @@
 # !=
 # <=
 # >=
-x = 5 > 10
-type(x)
-print(x)
+# x = 5 > 10
+# type(x)
+# print(x)
+# x = 1
+# print(x)
+# x = True
+# if x == 1 or x:
+#     print('yes')
+# elif x:
+#     print('Noway')
+# else:
+#     print('No')
+#
+# original_password = 'x777'
+# password = input('Введите пароль: ')
+# access = 0
+# if password == original_password:
+#     print('Пароль принят, добро пожаловать в систему')
+#     access = 1
+# else:
+#     print('Пароль неверен, вход запрещен')
+# color = 'red'
+# if color == 'blue':
+#     print('Синий')
+# elif color == 'red':
+#     print('Красный')
+# elif color == 'green':
+#     print('Зеленый')
+# else:
+#     print('Неизвестный цвет')
+
+# a = 0
+# while a < 7:
+#     print('A')
+#     a += 1
+
+x = input(' Say yes or no: ')
+while x == 'yes':
+    x=input(' Say yes or no: ')
+print('Yeeees')
